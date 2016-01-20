9a214ccf06ebb38fec8869ab0fa9292a46ad3f65
xadamus <adamus.w@gmail.com>
2016-01-20 20:53:20 +0100

    Nowa klasa - Activity. Poprawki CaloriesCalculator.

---
+++ src/Activity.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Adamus on 2016-01-20.
+ */
+public class Activity
+{
+    private String name;
+    private double ratio;
+}
--- src/CaloriesCalculator.java
+++ src/CaloriesCalculator.java
@@ -1,11 +1,23 @@
 import javax.swing.*;
 
 /**
- * Created by Micha³ Wrzesieñ on 2016-01-20.
+ * Created by Michaï¿½ Wrzesieï¿½ on 2016-01-20.
  */
-public class CaloriesCalculator
+public class CaloriesCalculator extends Calculator
 {
     private JTextField timeActivityField;
     private JComboBox activitiesComboBox;
     private JPanel CaloriesCalculator;
+
+    @Override
+    public BodyParams calculate()
+    {
+        BodyParams bodyParams = new BodyParams();
+        return bodyParams;
+    }
+
+    public void setActivity(Activity activity)
+    {
+
+    }
 }

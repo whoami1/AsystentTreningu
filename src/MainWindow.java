e2817ad8320c990db9bb985fa812d50e3d974004
xadamus <adamus.w@gmail.com>
2016-01-20 20:05:52 +0100

    Zmiany w interfejsie

---
+++ src/MainWindow.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+/**
+ * Created by Adamus on 2016-01-20.
+ */
+public class MainWindow {
+    private JTextField nameField;
+    private JRadioButton maleRadioButton;
+    private JRadioButton femaleRadioButton;
+    private JPanel StartPanel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+}

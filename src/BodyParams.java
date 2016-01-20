df3297844a8259ea9fb088db75d5ea5ad613d047
dscibors <mrbizq@gmail.com>
2016-01-20 22:33:04 +0100

    Dodawanie funkcji do BodyParamsCalc

--- src/BodyParams.java
+++ src/BodyParams.java
@@ -71,4 +71,42 @@ public class BodyParams
     {
         this.LBM = LBM;
     }
+
+    public String bmiClassification(double result)
+    {
+        String bmiClass = "";
+        if(result<16.0)
+        {
+            bmiClass = "wyglodzenie";
+        }
+        else if ((result>16.0) && (result<16.99))
+        {
+            bmiClass = "wychudzenie";
+        }
+        else if ((result>17.0) && (result<18.49))
+        {
+            bmiClass = "niedowaga";
+        }
+        else if ((result>18.5) && (result<24.99))
+        {
+            bmiClass = "wartosc prawidlowa";
+        }
+        else if ((result>25.0) && (result<29.99))
+        {
+            bmiClass = "nadwaga";
+        }
+        else if((result>30.0) && (result<34.99))
+        {
+            bmiClass = "I st otylosci";
+        }
+        else if((result>35.0) && (result<39.99))
+        {
+            bmiClass = "II st otylosci (otylosc kliniczna)";
+        }
+        else
+        {
+            bmiClass = "otylosc skrajna";
+        }
+        return bmiClass;
+    }
 }

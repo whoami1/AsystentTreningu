34ff443961bcc41eadbbcf61100bff1be58cb18c
xadamus <adamus.w@gmail.com>
2016-01-20 21:41:30 +0100

    Poprawki w CaloriesCalculator

--- src/CaloriesCalculator.java
+++ src/CaloriesCalculator.java
@@ -1,13 +1,28 @@
+import com.sun.xml.internal.ws.client.sei.ResponseBuilder;
+
 import javax.swing.*;
 
 /**
- * Created by Micha³ Wrzesieñ on 2016-01-20.
+ * Created by Michaï¿½ Wrzesieï¿½ on 2016-01-20.
  */
-public class CaloriesCalculator
+public class CaloriesCalculator extends Calculator
 {
     private JTextField timeActivityField;
     private JComboBox activitiesComboBox;
     private JPanel CaloriesCalculator;
     private JButton obliczButton;
     private JLabel resultLabel;
+
+    private Activity activity;
+
+    public BodyParams calculate()
+    {
+        BodyParams bodyParams = new BodyParams();
+        return bodyParams;
+    }
+
+    public void setActivity(Activity activity)
+    {
+
+    }
 }

7f9d214d9d5ca2925890b71bd89d53ff914637de
whoami <michal0504@gmail.com>
2016-01-20 20:18:55 +0100

    Kolejne okna - CaloriesCalculator + StartWindow

---
+++ src/CaloriesCalculator.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * Created by Micha³ Wrzesieñ on 2016-01-20.
+ */
+public class CaloriesCalculator
+{
+    private JTextField timeActivityField;
+    private JComboBox activitiesComboBox;
+    private JPanel CaloriesCalculator;
+}
---
+++ src/StartWindow.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+/**
+ * Created by Micha³ Wrzesieñ on 2016-01-20.
+ */
+public class StartWindow
+{
+    private JPanel StartPanel;
+    private JButton calcWaterFatBonesButton;
+    private JButton obliczenieIloœciKaloriiPotrzebnychButton;
+    private JButton porównanieWynikówButton;
+}
